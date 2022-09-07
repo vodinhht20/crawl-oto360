b744fa978af7f1de18bef92fcc5607d331a2d22f
Võ Định <vodinh2000ht@gmail.com>
2022-09-08 00:36:52 +0700

    update code

--- app/Http/Controllers/CrawlDataController.php
+++ app/Http/Controllers/CrawlDataController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Repositories\ProductImageRepository;
 use App\Repositories\ProductRepository;
+use DOMElement;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\Image;
 use Weidner\Goutte\GoutteFacade;
 
 class CrawlDataController extends Controller
@@ -24,32 +27,60 @@ class CrawlDataController extends Controller
         return view('main.index');
     }
 
-    public function index2(Request $request)
-    {
-        return view('main.index2');
-    }
-
     public function handleCrawl(Request $request)
     {
         $url = $request->domain;
+        try {
+            $crawler = GoutteFacade::request('GET', $url);
+            // $hasNoJsError = $crawler->filter('.no-js')->first()->text();
+            // dd($hasNoJsError);
+            // if ($hasNoJsError) {
+            //     return response()->json([
+            //         "status" => false,
+            //         "message" => "Trang web này đang bật tưởng lửa nên không thể crawl dữ liệu"
+            //     ], 422);
+            // }
 
-        $crawler = GoutteFacade::request('GET', $url);
+            // get title
+            $title = $crawler->filter('.product-info__header_title')->first()->text();
 
-        // get title
-        $title = $crawler->filter('.product-info__header_title')->each(function ($node) {
-            return $node->text();
-        });
+            // get size
+            $sizes = $crawler->filter('.product-info__variants_value-wrapper .product-info__variants_value .product-info__label')->each(function ($node) {
+                return $node->text();
+            });
 
-        // get title
-        $sizes = $crawler->filter('.product-info__variants_value-wrapper .product-info__variants_value .product-info__label')->each(function ($node) {
-            return $node->text();
-        });
+            // get color
+            $colors = $crawler->filter('.product-info__thumbnail .product-info__label_text')->each(function ($node) {
+                return $node->text();
+            });
 
-        $data = [
-            "title" => $title[0] ?? '',
-            "sizes" => $sizes ?? [],
+            // get price
+            $price = $crawler->filter('.product-info__header_price-wrapper .product-info__header_price')->first()->text();
 
-        ];
+            // get price
+            $description = $crawler->filter('.product-info__desc-tab-content.product_detail__content')->first()->html();
+
+            // get images
+            $images = $crawler->filter('.product-image .product-info__slide img')->each(function (Crawler $node) {
+                $element = $node->first();
+                return $element->attr("data-src") ?: $element->attr("src");
+            });
+
+            $data = [
+                "title" => $title ?? '',
+                "sizes" => $sizes ?? [],
+                "colors" => $colors,
+                "price" => $price,
+                "images" => $images,
+                "description" => $description,
+            ];
+        } catch (\Exception $e) {
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage()
+            ], 422);
+        }
+
         return response()->json($data);
     }
 }
