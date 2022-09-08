cb4bf2489026b9f10e45a23b23e66d6020571c5a
Võ Định <vodinh2000ht@gmail.com>
2022-09-08 23:57:35 +0700

    update code

--- app/Http/Controllers/CrawlDataController.php
+++ app/Http/Controllers/CrawlDataController.php
@@ -42,26 +42,32 @@ class CrawlDataController extends Controller
             // }
 
             // get title
-            $title = $crawler->filter('.product-info__header_title')->first()->text();
+            $title = $crawler->filter('.container .product-info__header_title')->first()->text();
 
             // get size
-            $sizes = $crawler->filter('.product-info__variants_value-wrapper .product-info__variants_value .product-info__label')->each(function ($node) {
-                return $node->text();
-            });
+            $sizes = $crawler->filter('.container .product-info__variants_value-wrapper')
+                ->last()
+                ->filter('.product-info__variants_value label')
+                ->each(function ($node) {
+                    return $node->text();
+                });
 
             // get color
-            $colors = $crawler->filter('.product-info__thumbnail .product-info__label_text')->each(function ($node) {
-                return $node->text();
-            });
+            $colors = $crawler->filter('.container .product-info__variants_value-wrapper')
+                ->first()
+                ->filter('.product-info__variants_value label')
+                ->each(function ($node) {
+                    return $node->text();
+                });
 
             // get price
             $price = $crawler->filter('.product-info__header_price-wrapper .product-info__header_price')->first()->text();
 
             // get price
-            $description = $crawler->filter('.product-info__desc-tab-content.product_detail__content')->first()->html();
+            $description = $crawler->filter('.product-info__desc-tab-content')->first()->html();
 
             // get images
-            $images = $crawler->filter('.product-image .product-info__slide img')->each(function (Crawler $node) {
+            $images = $crawler->filter('.product-image .swiper-slide img')->each(function (Crawler $node) {
                 $element = $node->first();
                 return $element->attr("data-src") ?: $element->attr("src");
             });
